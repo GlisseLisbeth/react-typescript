1e4e8e202df20a18e68232b35c1d4ecbeabec84c
GlisseLisbeth <glisselisbeth@gmail.com>
2019-03-07 16:43:51 -0500

    IProps and IState

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,25 @@
 import * as React from "react";
 
-export class App extends React.Component<any, any> {
+export class App extends React.Component<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      tasks: []
+    };
+  }
+
   render() {
     return (
       <h1>{this.props.title}</h1>
     )
   }
+}
+
+interface IProps {
+  title: string;
+}
+
+interface IState {
+  tasks: [];
 }
